e5ec3350b26267a0ab9e2a42120a1f9e9b7e8a28
Brendan Hellar and Tessa Sullivan <tessa@>
2019-03-28 09:22:32 -0700

    User is asked age and site responds with whether or not they can vote

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
+
 $(document).ready(function() {
   $("form#voting").submit(function(event) {
-
-    event.preventDefault();
     var age = parseInt($("#age").val());
     if (age < 18) {
-      alert("You can't vote yet");
+      $("#canvote").hide();
+      $("#cantvote").show();
     } else {
-      alert("You can vote");
+      $("#canvote").show();
+      $("#cantvote").hide();
+
     }
+    event.preventDefault();
   });
 });
