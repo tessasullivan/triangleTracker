ec49a92f001ba1b7e28b349c9fbe1e8072ea4bc5
Brendan Hellar and Tessa Sullivan <tessa@>
2019-03-28 13:02:14 -0700

    Initial code to ask user for 3 values. Can distinguish between eq, iso, and scalene triangles

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,21 @@
 
 $(document).ready(function() {
-  $("form#quiz").submit(function(event) {
-    var age = parseInt($("#age").val());
-    var sex = $("#sex").val();
-    var orientation = $("#orientation").val();
-    var match;
-    var matchImage;
-
-    if (age) {
-      if (age < 30) {
-        // Harry Potter characters
-        if ((orientation === 'Straight' && sex === 'Male')
-          || (orientation === 'Gay' && sex === 'Female')) {
-            match = 'Hermione Granger';
-            matchImage = 'img/HermioneGranger.jpg';
-          } else if ((orientation === 'Gay' && sex === 'Male')
-          || (orientation === 'Straight' && sex === 'Female')){
-            match = 'Harry Potter';
-            matchImage = 'img/HarryPotter.jpg';
-
-          }
+  $("form#triangle").submit(function(event) {
+    var sideA = parseInt($("#sideA").val());
+    var sideB = parseInt($("#sideB").val());
+    var sideC = parseInt($("#sideC").val());
+    if (sideA && sideB && sideC) {
+      if (sideA === sideB && sideB === sideC) {
+        $("#output").append("This is an equilatoral triangle.");
+      } else if (sideA === sideB || sideA === sideC || sideB === sideC){
+        $("#output").append("This is an isoceles triangle.");
       } else {
-        //Game of Thrones characters
-        if ((orientation === 'Straight' && sex === 'Male')
-          || (orientation === 'Gay' && sex === 'Female')) {
-            match = 'Daenerys Targaryen';
-            matchImage = 'img/DaenerysTargaryen.jpg';
-
-          } else if ((orientation === 'Gay' && sex === 'Male')
-          || (orientation === 'Straight' && sex === 'Female')){
-            match = 'Khal Drogo';
-            matchImage = 'img/KhalDrogo.jpg';
-
-          }
-          // alert("match is " + match);
+        $("#output").append("Not");
       }
-        $("#quiz").hide();
-       $("#match").prepend("<h1>Your match is " + match + "</h1>");
-       $("#match").append("<img src=" + matchImage + ">");
     } else {
-      alert("Please enter your age");
+      //User left a field blank
+      alert("Please enter values for all 3 sides.");
     }
-
     event.preventDefault();
   });
 });
